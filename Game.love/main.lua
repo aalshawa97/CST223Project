ff8fec5c46190c7b07bd667cdf555a0f0fe24b76
aalshawa97 <abdullahalshawa@gmail.com>
2018-02-17 14:56:52 -0800

    Added Enemy Movement Patterns, Set Ship Boundaries, Edited Firing Audio, Changed Ship Spawn Point

--- Game.love/main.lua
+++ Game.love/main.lua
@@ -7,11 +7,10 @@
 --Initialize values for game scene
 local welcomeImage
 function love.load()
-  game_over = false
   welcomeImage = love.graphics.newImage("images/startScreen.jpg")
   player = {}
-  player.x = 0
-  player.y = 550
+  player.x = 345
+  player.y = 540
   player.bullets = {}
   player.cooldown = 20
   player.speed = 10
@@ -28,10 +27,8 @@ function love.load()
       table.insert(player.bullets, bullet)
     end
  end
-  
-  for i=0, 8 do
-  	enemy_controller:spawnEnemy(i*10,0)
-  end
+  enemy_controller:spawnEnemy(300,0)
+  enemy_controller:spawnEnemy(0,0)
 end
 
 --Enemy fire code
@@ -60,13 +57,14 @@ end
 function enemy_controller:spawnEnemy(x,y)
   --Initialize enemy properties
   enemy = {}
-  enemy.x = 0
+  enemy.x = 50
   enemy.y = 0
   enemy.width = 10
   enemy.height = 10
   enemy.bullets = {}
   enemy.cooldown = 20
   enemy.speed = 10
+  enemy.movecount = 0
   table.insert(self.enemies,enemy)
 end
 
@@ -76,10 +74,11 @@ function love.update(dt)
   player.cooldown = player.cooldown - 1
 
 --Handle player controls
-  if love.keyboard.isDown("right") then
-    player.x = player.x + player.speed
-  elseif love.keyboard.isDown("left") then
-    player.x = player.x - player.speed
+  if ( (love.keyboard.isDown("right") or love.keyboard.isDown("d"))  and (player.x <= love.graphics.getWidth() - 70) )   then
+    player.x = player.x + 3
+  
+  elseif ((love.keyboard.isDown("left") or love.keyboard.isDown("a")) and (player.x >= 10)) then
+    player.x = player.x - 3
   end
 
   if love.keyboard.isDown("space") then
@@ -88,10 +87,59 @@ function love.update(dt)
 
   --Enemy movement
   	for _,e in pairs(enemy_controller.enemies)do
-  		if e.y >= love.graphics.getHeight()/4 then
-  			game_over = true
-  		end
-  		e.y = e.y + 1
+      --****straight downward movement***
+  		--e.y = e.y + 1
+      --****end straight downward movement****
+      
+      --****below code creates a stairstep movement for enemy****
+      --if enemy movecount < 50 move down
+      if e.movecount <= 50 then
+        e.y = e.y + 1
+        e.movecount = e.movecount + 1
+      --else if movecount between 50 and 100 move right
+      elseif (e.movecount > 50) and  (e.movecount <= 100) then
+        e.x = e.x + 1
+        e.movecount = e.movecount + 1
+      end
+      --reset movecount
+      if e.movecount > 100 then
+        e.movecount = 0
+      end
+      --****end stairstep movement***
+      
+      
+      --****below code creates an offset zigzag movement for enemy****
+      --[[--enemy move down
+      if e.movecount <= 20 then
+        e.y = e.y + 1
+        e.movecount = e.movecount + 1
+      --move right
+      elseif (e.movecount > 20) and  (e.movecount <= 40) then
+        e.x = e.x + 1
+        e.movecount = e.movecount + 1
+      --move down
+      elseif (e.movecount > 40) and  (e.movecount <= 60) then
+        e.y = e.y + 1
+        e.movecount = e.movecount + 1
+      --move left twice as far
+      elseif (e.movecount > 60) and  (e.movecount <= 80) then
+        e.x = e.x - 2
+        e.movecount = e.movecount + 1
+      --move down
+      elseif (e.movecount > 80) and  (e.movecount <= 100) then
+        e.y = e.y + 1
+        e.movecount = e.movecount + 1
+      --move right
+      elseif (e.movecount > 100) and  (e.movecount <= 120) then
+        e.x = e.x + 1
+        e.movecount = e.movecount + 1
+      end
+      --reset movecount
+      if e.movecount > 120 then
+        e.movecount = 0
+      end
+      --****end offset zigzag movement code****--]]
+      
   	end
 
   for i,b in ipairs(player.bullets) do
@@ -104,18 +152,11 @@ function love.update(dt)
 end
 
 function love.draw()
-   --Check if the game is over
-   if game_over then	
-	   love.graphics.print("Game Over",300,300,0,2,2,0,0)
-	   --Do this so nothing else can be printed on the screen
-	   return
-   end
-
   --Draw the welcome image
-  --love.graphics.draw(welcomeImage, 54, 54)
- 
+  love.graphics.draw(welcomeImage, 54, 54)
+
   --Draw the player
-  love.graphics.setColor(0, 0, 100)
+  love.graphics.setColor(255, 255, 255)
   love.graphics.draw(player.image, player.x, player.y,0,.2)
 
   love.graphics.setColor(255,255,255)
@@ -127,6 +168,6 @@ function love.draw()
 -- Draw bullets
   love.graphics.setColor(255,0,0)
   for _,b in pairs(player.bullets) do
-    love.graphics.rectangle("fill", b.x-17, b.y, 10, 10)
+    love.graphics.rectangle("fill", b.x, b.y, 10, 10)
   end
 end
